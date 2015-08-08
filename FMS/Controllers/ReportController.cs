a1e87615d7c8988a5704fd820ad46a3c5a7faa41
Oleg Panikarovskiy <o.pnk@synergetica.net>
2015-08-09 00:09:21 +0300

    test push

---
+++ FMS/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstract;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMS.Controllers
+{
+	public class ReportController : Controller
+	{
+		private readonly IRepository<Document> _repDocs;
+		private readonly IRepository<DocumentParameter> _repDocParams;
+		private readonly IRepository<ParameterName> _repDocParamNames;
+
+		public ReportController(IRepository<Document> repDocs, IRepository<DocumentParameter> repDocParams, IRepository<ParameterName> repDocParamNames)
+		{
+			_repDocParamNames = repDocParamNames;
+			_repDocParams = repDocParams;
+			_repDocs = repDocs;
+        }
+
+		// GET: Report
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
