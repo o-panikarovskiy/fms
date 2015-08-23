9715b61238a4c4c81a9ea286e3b8001b89db06a2
Oleg Panikarovskiy <oleg.pnk@gmail.com>
2015-08-23 22:03:30 +0300

    params list output

--- FMS/js/controllers/SearchCtrl.js
+++ FMS/js/controllers/SearchCtrl.js
@@ -1,7 +1,7 @@
-﻿(function (window, angular) {
+﻿(function(window, angular) {
     'use strict';
 
-    angular.module('fms').controller('SearchCtrl', ['$scope', '$state', '$q', 'DictionaryService', 'SearchService', function ($scope, $state, $q, DictionaryService, SearchService) {
+    angular.module('fms').controller('SearchCtrl', ['$scope', '$state', '$q', 'DictionaryService', 'ParameterService', 'SearchService', function($scope, $state, $q, DictionaryService, ParameterService, SearchService) {
         var model = {
             person: {
                 birthday: null,
@@ -31,20 +31,20 @@
         $scope.vm = { loader: {} };
         $scope.searchModel = angular.copy(model);
 
-        $scope.search = function () {
+        $scope.search = function() {
             $scope.vm.isSendingRequest = true;
-            return SearchService.query(bindModel($scope.searchModel)).then(function (data) {
+            return SearchService.query(bindModel($scope.searchModel)).then(function(data) {
                 $state.go('root.search.results', { id: data.id });
-            }).finally(function () {
+            }).finally(function() {
                 $scope.vm.isSendingRequest = false;
             });
         }
 
-        $scope.reset = function () {
+        $scope.reset = function() {
             $scope.searchModel = angular.copy(model);
         }
 
-        $scope.loadDict = function (name, docType, categody, vm) {
+        $scope.loadDict = function(name, docType, categody, vm) {
             vm = vm || $scope.vm;
             if (!vm.dicts[name]) {
                 loadDict(name, docType, categody, vm);
@@ -55,7 +55,7 @@
             var m = angular.copy(model);
 
             if (m.docs) {
-                var isAnyChecked = Object.keys(m.docs).some(function (key) {
+                var isAnyChecked = Object.keys(m.docs).some(function(key) {
                     return m.docs[key] && m.docs[key].isChecked
                 });
                 if (!isAnyChecked) {
@@ -67,12 +67,38 @@
         }
 
         function loadDict(name, docType, categody, vm) {
-            return DictionaryService.get(name, docType, categody, vm || $scope.vm);
+            $scope.vm.loader.dict = true;
+            return DictionaryService.get(name, docType, categody, vm || $scope.vm).finally(function () {
+                $scope.vm.loader.dict = false;
+            });
+        }
+
+        function loadDocParams() {
+            $scope.vm.loader.dict = true;
+            return ParameterService.list().then(function(list) {
+                var prms = {};
+                list.map(function(item) {
+                    if (!angular.isArray(prms[item.docType])) {
+                        prms[item.docType] = [];
+                    }
+                    prms[item.docType].push(item);
+                    return item;
+                });
+                $scope.parameters = prms;
+            }).finally(function() {
+                $scope.vm.loader.dict = false;
+            });
         }
 
         function init() {
             $scope.vm.loader.dicts = true;
-            $q.all([loadDict('personCategory'), loadDict('personType'), loadDict('Гражданство', null, 'individual'), loadDict('Личный документ', null, 'individual')]).finally(function () {
+            $q.all([
+                loadDict('personCategory'),
+                loadDict('personType'),
+                loadDict('Гражданство', null, 'individual'),
+                loadDict('Личный документ', null, 'individual'),
+                loadDocParams()
+            ]).finally(function() {
                 $scope.vm.loader.dicts = false;
             });
         }
